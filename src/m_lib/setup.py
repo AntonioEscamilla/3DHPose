99daec94da82b1a26ac3544ee2835c036ba120e4
Antonio Escamilla <7999008+AntonioEscamilla@users.noreply.github.com>
2022-02-15 19:48:13 +0100

    Update setup.py for pictorial function

--- src/m_lib/setup.py
+++ src/m_lib/setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 ext_modules=[ Extension("pictorial",
-              ["pictorial.pyx"],
-              libraries=["m"],
-              extra_compile_args = ["-ffast-math", "-O3"])]
+             ["pictorial.pyx"],
+             include_dirs=[numpy.get_include()],
+             libraries=["m"],
+             extra_compile_args = ["-ffast-math", "-O3"])]
 
 setup(
   name = "pictorial",
